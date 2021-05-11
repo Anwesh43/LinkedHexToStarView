dd431f3e26b117ee37b5701f9a2cec8c555e719e
Anwesh43 <anweshthecool0@gmail.com>
2021-05-12 01:16:31 +0530

    adding constants and libraries

--- hextostarview/src/main/java/com/example/hextostarview/HexToStarView.kt
+++ hextostarview/src/main/java/com/example/hextostarview/HexToStarView.kt
@@ -1,2 +1,26 @@
 package com.example.hextostarview
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.content.Context
+import android.app.Activity
+
+val colors : Array<Int> = arrayOf(
+    "#f44336",
+    "#2196F3",
+    "#00BFA5",
+    "#BF360C",
+    "#311B92"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 3.2f
+val deg : Float = 60f
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
