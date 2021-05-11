8cc0e75b96539318a3eb0d08025baeb89c7d4ed6
Anwesh43 <anweshthecool0@gmail.com>
2021-05-12 01:37:23 +0530

    adding HTSNode class

--- hextostarview/src/main/java/com/example/hextostarview/HexToStarView.kt
+++ hextostarview/src/main/java/com/example/hextostarview/HexToStarView.kt
@@ -124,4 +124,45 @@ class HexToStarView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class HTSNode(var i : Int, val state : State = State()) {
+
+        private var next : HTSNode? = null
+        private var prev : HTSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = HTSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawHTSNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : HTSNode {
+            var curr : HTSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
